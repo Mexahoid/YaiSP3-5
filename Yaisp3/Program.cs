778e2d77795f7cfc13810b2562f074a925cea56b
Mexahoid <mirramax97@Mail.ru>
2016-11-02 18:56:07 +0300

    Upd

--- Yaisp3/Program.cs
+++ Yaisp3/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Yaisp3
+namespace Yaisp3_v._0._1
 {
   static class Program
   {
+    public static _FormAgency formCreateAgency;
+    public static _FormMain formMain;
+    public static _FormCity formCity;
     /// <summary>
-    /// Главная точка входа для приложения.
+    /// The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main()
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+      formMain = new _FormMain();
+      formCreateAgency = new _FormAgency();
+      formCity = new _FormCity();
+      Application.Run(formMain);
     }
   }
 }
--- Yaisp3/Properties/AssemblyInfo.cs
+++ Yaisp3/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
-[assembly: AssemblyTitle("Yaisp3")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Yaisp3_v._0.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yaisp3")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("VSU, CS")]
+[assembly: AssemblyProduct("Yaisp3_v._0.1")]
+[assembly: AssemblyCopyright("Copyright © VSU, CS 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
-// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("fcab7d9d-8268-4128-8a8d-7826689ec069")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ff6a35b-f657-4dd4-a52e-c31fe78273da")]
 
-// Сведения о версии сборки состоят из следующих четырех значений:
+// Version information for an assembly consists of the following four values:
 //
-//      Основной номер версии
-//      Дополнительный номер версии 
-//   Номер сборки
-//      Редакция
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
